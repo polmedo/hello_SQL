4639cad0788904155590127d73f46f8838a3dd6f
PabloPuente <pablolmedo94@gmail.com>
2025-03-11 22:41:21 +0000

    Updated tables and created relations

---
+++ statements/relations.sql
@@ -0,0 +1,5 @@
+-- IMPORTANTE Las relaciones siempre se harán usando la clave primaria de la tabla relacionada.
+
+-- Para crear una relación n:n, normalmente se crea una tabla intermedia que contiene las relaciones entre las dos tablas.
+
+INSERT INTO users (sign) VALUES ('Aries') WHERE name = 'Sofia';
--- statements/tables.sql
+++ statements/tables.sql
@@ -28,3 +28,29 @@ RENAME COLUMN user_id TO id;
 
 ALTER TABLE users
 MODIFY COLUMN name varchar(60);
+
+UPDATE users SET sign = 'Leo' WHERE id = 1;
+UPDATE users SET sign = 'Aries' WHERE id = 3;
+UPDATE users SET sign = 'Virgo' WHERE id = 4;
+UPDATE users SET sign = 'Gemini' WHERE id = 2;
+UPDATE users SET sign = 'Piscis' WHERE id = 5;
+
+ALTER TABLE users
+RENAME COLUMN id TO user_id;
+
+ALTER TABLE signs2
+RENAME COLUMN id TO sign_id;
+
+SELECT * FROM signs2;
+
+INSERT INTO signs2 (sign, attributes) VALUES ('Gemini', 'The most beautiful and intelligent and funny');
+INSERT INTO signs2 (sign, attributes) VALUES ('Leo', 'The luckiest');
+INSERT INTO signs2 (sign, attributes) VALUES ('Aries', 'The worst');
+INSERT INTO signs2 (sign, attributes) VALUES ('Virgo', "It's ok");
+INSERT INTO signs2 (sign, attributes) VALUES ('Piscis', 'The most normal');
+
+ALTER TABLE signs2
+ADD user_id int;
+
+ALTER TABLE signs2
+ADD FOREIGN KEY (user_id) REFERENCES users(user_id);
