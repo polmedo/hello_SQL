9fef97625da9bcf33899618678b3df826a837219
PabloPuente <pablolmedo94@gmail.com>
2025-03-11 21:54:19 +0000

    More tables

--- statements/tables.sql
+++ statements/tables.sql
@@ -6,9 +6,25 @@ CREATE TABLE signs (
 );
 
 -- This way what is unique is the combination of id and sign.
-CREATE TABLE signs (
+CREATE TABLE signs2 (
     id int NOT NULL,
     sign varchar(20) NOT NULL,
     attributes varchar(200) NOT NULL,
     UNIQUE (id, sign)
 );
+
+CREATE TABLE signs3 (
+	id int NOT NULL UNIQUE AUTO_INCREMENT,
+    sign varchar(20) NOT NULL UNIQUE,
+    attributes varchar(200) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE users
+ADD sign varchar(20) NOT NULL;
+
+ALTER TABLE users
+RENAME COLUMN user_id TO id;
+
+ALTER TABLE users
+MODIFY COLUMN name varchar(60);
