6ab411b4ef7a1e5786e82aec8e594adc7943e2ed
PabloPuente <pablolmedo94@gmail.com>
2025-03-13 22:55:04 +0000

    Relations 1:1 and 1:n

--- statements/relations.sql
+++ statements/relations.sql
@@ -2,4 +2,18 @@
 
 -- Para crear una relación n:n, normalmente se crea una tabla intermedia que contiene las relaciones entre las dos tablas.
 
-INSERT INTO users (sign) VALUES ('Aries') WHERE name = 'Sofia';
+-- Relación 1:1
+CREATE TABLE dni (
+	dni_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dni_number varchar(10) NOT NULL,
+    user_id int NOT NULL,
+    UNIQUE(dni_id),
+    FOREIGN KEY(user_id) REFERENCES users(user_id)
+    );
+
+ALTER TABLE users
+ADD sign_id int;
+
+ALTER TABLE users
+ADD CONSTRAINT fk_signs
+FOREIGN KEY(sign_id) REFERENCES signs2(sign_id);
